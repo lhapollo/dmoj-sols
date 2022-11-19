132d24871f3bbf4436686f93458cb0c8c1d7741c
Lexi Han <108366350+lhapollo@users.noreply.github.com>
2022-11-19 13:05:26 -0500

    Update oly18novp3.cpp

--- oly18novp3.cpp
+++ oly18novp3.cpp
@@ -1,58 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 typedef long long ll;
-ll ans = 10e14;
+
 vector<ll> arr(100001);
+ll ans = 0, lo = 0, hi = 0, mid = 0;
 int n, m;
 
-bool valid(ll mid, vector<ll>& arr, int n, int m){
-    int count = 0;
-    ll sum = 0;
+//a method to check if it's possible to create at most m subarrays with subarray sums less than val
+bool valid(ll val){
+    int count = 1; //keeps track of number of subarrays
+    ll subArrSum = 0; //keeps track of sum of current subarray.
     for (int i = 1; i <= n; i++){
-        if (arr[i] > mid){
-            return false;
-        }
-        sum += arr[i];
-        if (sum > mid){
+        //if (arr[i] > val) return false;
+        if (subArrSum + arr[i] <= val) subArrSum += arr[i]; //add arr[i] to current subarray, updating the sum of current subarray.
+        else {
+            //reset, increment number of subarrays by 1. 
             count++;
-            sum = arr[i];
+            subArrSum = arr[i];
         }
     }
-    
-    count++;
-    if (count <= m){
-        return true;
-    }
-    return false;
-}
-
-ll solve(vector<ll>& arr, int n, int m){
-    ll hi = max_element(arr.begin(), arr.end())-arr.begin();
-    ll start = hi;
-    ll end = 0;
-    for (int i = 1; i <= n; i++){
-        end += arr[i];
-    }
-    ll ans = 0;
-    while (start <= end){
-        ll mid = (start+end)/2;
-        if (valid(mid,arr,n,m)){
-            ans = mid;
-            end = mid-1;
-        } else{
-            start = mid+1;
-        }
-    }
-    return ans;
+    return count <= m;
 }
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cin >> n >> m;
+    //find the maximum possible subarray sum, as well as minimum possible subarray sum.
     for (int i = 1; i <= n; i++){
         cin >> arr[i];
+        lo = max(lo, arr[i]);
+        hi += arr[i];
+        
     }
-    cout << solve(arr,n,m);
+    //binary search, to find the optimal minimum maximum subarray sum.
+    while (lo <= hi){
+        mid = (lo+hi)/2;
+        //if the middle value can be a possible max subarray sum, it is our new minimum, and our new answer.
+        if (valid(mid)){
+            ans = mid;
+            hi = mid-1;
+        }
+        else {
+            lo = mid+1;
+        }
+    }
+    cout << ans << "\n";
+    //return 0;
 }
